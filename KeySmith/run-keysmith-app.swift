644cb898d60585dfa284f7c78546ee752fc5edc9
DouglasCodesEk <douglasek@proton.me>
2024-10-16 11:52:27 +0200

    Delete KeySmith directory

--- KeySmith/run-keysmith-app.swift
+++
@@ -1,6 +0,0 @@
-// In Xcode:
-// 1. Select your target device (Mac or iOS Simulator)
-// 2. Click the "Run" button (play icon) or use the keyboard shortcut Cmd + R
-
-// You can also run from the command line:
-xcodebuild -scheme KeySmith -configuration Debug build
