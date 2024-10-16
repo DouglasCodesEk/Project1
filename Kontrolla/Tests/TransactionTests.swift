073193f7e85b119aa8c0734f28c13ddeb7c53366
DouglasCodesEk <douglasek@proton.me>
2024-10-16 11:52:39 +0200

    Delete Kontrolla directory

--- Kontrolla/Tests/TransactionTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import SvenskBokforingAI
-
-class TransactionTests: XCTestCase {
-    func testVATCalculation() {
-        let transaction = Transaction(date: "2023-10-06", description: "Test", amount: 100.0, vatRate: .standard)
-        XCTAssertEqual(transaction.vatAmount, 20.0, accuracy: 0.01)
-    }
-}
